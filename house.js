0945eb4422bb30a8a2f1262310adec0ab4e8fa4d
abdurahim <115877955+abushshsgi@users.noreply.github.com>
2024-01-10 05:25:34 +0500

    Abdu Figma

---
+++ house.js
@@ -0,0 +1,16 @@
+let information = alert("Salom mening Saytimga xush kelibsiz :");
+
+let Name = prompt ("Ismingizni Kiriting !");
+console.log(Name);
+
+let name2 = alert (`${Name} ismingiz chiroyli ekan`);
+let love = prompt(`${Name}  Qachon IELTH topshirasiz  ? `);
+console.log(love);
+
+let Ielth = prompt (`Nechchi olishni xohlaysiz  5 6 7 8 9 `)
+console.log(Ielth)
+
+let yaxshi = alert(` Gap yuq sizga  ${Name } Niyatizga erishin iloxim !! `)
+let zur = prompt(` Meni yaxshi kurasizmi ${Name}   Xa / Yuk `)
+console.log(zur)
+let love2 = alert( `"Men xam sizni Yaxshi kuraman ${Name} ` ) 
